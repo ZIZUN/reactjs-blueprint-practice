91ff0205f08562adca1320648ed5c3e700784d25
ZIZUN <cap1232@naver.com>
2020-01-20 02:48:22 +0900

    dd
    dd

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -5,6 +5,7 @@ import {loginAction} from '../../actions/login'
 import {IsValidForm} from '../../components/validation'
 import { AnchorButton, Button, Code, H5, Intent, Switch } from "@blueprintjs/core";
 import logo from './logo1.png';
+import google_login from './google_login.png';
 import './App.css';
 
 class Login extends React.Component{
@@ -68,16 +69,18 @@ class Login extends React.Component{
         <form className="aform" style={{ backgroundColor: '#f8f361'}}  onSubmit={this.onSubmit.bind(this)}>
           <img style={{ margin: '0 auto', marginTop:'40px' }} src={logo} className="App-logo"  alt="logo" />
 
-          <div style={{width: '420px',margin: '0 auto',marginTop: '20px',padding: '50px'}}>
+          <div style={{width: '380px',margin: '0 auto',marginTop: '20px',padding: '50px'}}>
             <div style={{height: '70px'}}>
-              <input className="pt-input" style={{width: "320px",height: '50px'}}  placeholder="아이디" type="text" dir="auto" onChange={this.onChange.bind(this,'email')}/>
+              <input className="pt-input" style={{width: "280px",height: '50px'}}  placeholder="아이디" type="text" dir="auto" onChange={this.onChange.bind(this,'email')}/>
               {!!this.showError('email') ? <p className="error-message">{this.getError('email')} </p> : null}
             </div>
             <div style={{height: '70px'}}>
-              <input className="pt-input" style={{width: "320px",height: '50px'}} placeholder="비밀번호" type="password" dir="auto" onChange={this.onChange.bind(this,'password')}/>
+              <input className="pt-input" style={{width: "280px",height: '50px'}} placeholder="비밀번호" type="password" dir="auto" onChange={this.onChange.bind(this,'password')}/>
               {!!this.showError('password') ? <p className="error-message">{this.getError('password')} </p> : null}
             </div>
-            <button class="pt-button pt-intent-primary" style={{ width: 320,height: '50px'}} type="submit" >로그인</button>
+            <button class="pt-button pt-intent-primary" style={{ width: 280,height: '50px'}} type="submit" >로그인</button>
+            <button class="pt-button pt-intent-primary" style={{ marginTop:10, width: 250,height: '50px'}}><img style={{ width: 250,height: '50px'}} src={google_login}/></button>
+
             {this.state.errorMsg != '' && <p className="error-message" style={{textAlign: 'center', marginTop: 20}}>{this.state.errorMsg}</p>}
 
           </div>
